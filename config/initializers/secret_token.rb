34cce316f4a1966bfb545c7e4a55a2e006cf92ff
root <root@pacifique.serveurs.dsi.univ-fcomte.fr>
2013-03-12 12:11:05 +0100

    creation projet Evarisk avec mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Evarisk::Application.config.secret_token = '13c31fe4c88cd0b008d3a62b57742c8a59dc9dfe8c03817a04f9e1c6783e294839e76922e4f6f0f1b9b07489097f946356bd499005f2c842a8166bf63fc738af'
